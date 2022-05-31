76a972767d74470a217497f779ac2d4ebd367e4f
lee9805 <dlthwjd1161@naver.com>
2022-05-31 15:47:57 +0900

    NaverSearch

--- SpMVC_004_Naver/src/main/java/com/callor/naver/config/NaverConfigSample.java
+++ SpMVC_004_Naver/src/main/java/com/callor/naver/config/NaverConfigSample.java
@@ -1,5 +1,11 @@
 package com.callor.naver.config;
 
+/*
+ * Naver 개발자센터에 등록하고
+ * 애플리케이션을 생성하여 부여된 ID와 SEC 를
+ * 다음의 변수에 작성하고 이 클래스를 naverConfig 이름으로
+ * 변경한 후 프로젝트를 실행하시오
+ */
 public class NaverConfigSample {
 
 	public static final String NAVER_CLIENT_ID = "";
