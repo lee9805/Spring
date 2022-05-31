4929f06a700e4d7c96455742f0057eaf1d3bbeee
lee9805 <dlthwjd1161@naver.com>
2022-05-31 09:22:35 +0900

    NaverSearch

--- SpMVC_003_SchoolV7/README.md
+++ SpMVC_003_SchoolV7/README.md
@@ -10,3 +10,52 @@
 * 만약 button을 클릭했을때 Js코드를 사용하여 상황을 제어 하고 싶으면 button type 을 ```button```으로 설정한다
 * button type 을 ```button``` 으로 설정하면 모양만 button이고 아무런 역할을 수행하지 못한다
 * button type에는 ```reset```도 있는데 이 type을 form tag에 포함된 input box 데이터를 모두 제거한다
+
+## password 암호화 하기
+* Spring-seccurity-core 가져오기
+```
+<dependency>
+	<groupId>org.springframework.security</groupId>
+	<artifactId>spring-security-core</artifactId>
+	<version>5.3.13.RELEASE</version>
+</dependency>
+```
+* Spring/appServlet/security-context.xml 파일
+
+```
+<bean id="passwordEncoder" class="org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder">
+	<constructor-arg index="0" value="4"/>
+</bean>
+```
+* PasswordEncoder 를 상속받은 BCryptPasswordEncoder 클래스를 사용하여 bean 선언
+* 이때 생성자에 설정한 value 값에 따라 내부에서 암호화 반복이 수행된다
+* 이 값은 4이상의 정수로 설정한다
+* 기본값은 4이며, 값이 커지면 암호화에서 시간이 많이 소요된다
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SpMVC_003_SchoolV7/src/main/java/com/callor/spring/service/impl/UserServiceImplV1.java
+++ SpMVC_003_SchoolV7/src/main/java/com/callor/spring/service/impl/UserServiceImplV1.java
@@ -2,6 +2,7 @@ package com.callor.spring.service.impl;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.callor.spring.model.UserVO;
@@ -11,13 +12,16 @@ import com.callor.spring.service.UserService;
 /*
  * UserServiceImplV1 클래스를 Component(bean) 로 등록하기
  */
-@Service
+@Service("userServiceV1")
 public class UserServiceImplV1 implements UserService {
 
-	private final UserDao userDao;
+	//security-context.xml 에 설정된 bean 불러오기
+	protected final PasswordEncoder pass;
+	protected final UserDao userDao;
 	
-	public UserServiceImplV1(UserDao userDao) {
+	public UserServiceImplV1(UserDao userDao, PasswordEncoder pass){
 		this.userDao = userDao;
+		this.pass= pass;
 	}
 	
 	@Override
@@ -41,9 +45,16 @@ public class UserServiceImplV1 implements UserService {
 	@Override
 	public UserVO join(UserVO userVO) {
 		
+		//회원가입한 user의 정보를 저장하기 전에
+		//비밀번호흫 암호화 하자
+		 String encPassword = pass.encode(userVO.getPassword());
+		 userVO.setPassword(encPassword);
+		
+		
 		//1.user table 에 데이터가 있는지 확인하기 위하여
 		//		전체 데이터를 select 하기
 		List<UserVO> userList = userDao.selectAll();
+		// 만약 user table 에 데이터가 없으면 null 을 return한다
 		if(userList == null || userList.size() < 1) {
 			//데이터가 없으면 최초 가입자 이므로
 			//role(권한)을 ADMIN(관리자)로 부여한다
