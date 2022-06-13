990c06897418e926584a8fdba14034da2638afb9
lee9805 <dlthwjd1161@naver.com>
2022-06-13 15:56:15 +0900

    db

---
+++ SpMVC_005_GalleryV1/src/main/java/com/callor/images/model/UserVO.java
@@ -0,0 +1,26 @@
+package com.callor.images.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class UserVO  {
+
+	private String username;
+	private String password;
+	private String email;
+	private String name;
+	private String nickname;
+	private String role;
+	
+	
+}
